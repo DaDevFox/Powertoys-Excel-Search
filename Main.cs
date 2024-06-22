97d5ae0ac0474ec51a64acee4cccfafc8370bf2d
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-06-22 11:30:27 -0400

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Community.Powertoys.Run.Plugin.ExcelSearch
+{
+    public class Main
+    {
+    }
+}
